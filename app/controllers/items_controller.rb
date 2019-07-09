201a571fb87e6027104b714b302efb999a068824
Satoshi Shimizu <3104shimizu@SatoshinoMacBook-Air.local>
2019-07-09 22:07:10 +0900

    Add new item function

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,18 +4,20 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @items = Item.new
     @languages  = Language.all
     @jobtypes   = Usertype.all
     @industries = Industry.all
   end
 
   def create
-    item = Item.create(item_params)
+    #item = Item.create(item_params)
   end
 
   private
   def item_params
-    params.permit(:title, :contents, :price_s, :price_e, :place_id, :jobtype_id, :industry_id)
+    params.require("item").permit(:title, :contents, :price_s, :price_e, :place_id, :jobtype_id, :industry_id)
+    binding.pry
   end
   
 end
