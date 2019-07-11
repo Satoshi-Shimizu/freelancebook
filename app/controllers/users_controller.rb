ed3cba1313e3a322c869cd6eff3242fe62a3691f
Satoshi Shimizu <3104shimizu@SatoshinoMacBook-Air.local>
2019-07-11 23:57:50 +0900

    Add user page function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
+
   def show
-    @name = current_user.name
-    @items = current_user.items.where(user_id: current_user.id).page(params[:page]).per(5).order("created_at DESC")
+    @user = current_user
+    @items = Item.where(user_id: current_user.id).page(params[:page]).per(5).order("created_at DESC")
   end
 
 end
